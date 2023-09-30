b2db8be19fe268e5501efd4ad5eb73219bc05673
Alyson Franco <alysonfranco4@gmail.com>
2023-09-30 01:20:26 -0400

    added styling

--- PassengerCounter/index.js
+++ PassengerCounter/index.js
@@ -1,16 +1,15 @@
 let count = 0;
 let countEl = document.getElementById("count-el");
-let saveEl = document.getElementById("save-el")
+let saveEl = document.getElementById("save-el");
 
 function increment() {
-    count++;
-    countEl.innerText = count;
-    console.log("clicked");
+  count++;
+  countEl.innerText = count;
 }
 
 function save() {
-    let entries = count + " - ";
-    saveEl.textContent += entries;
-    console.log(count);
+  let entries = count + " - ";
+  saveEl.textContent += entries;
+  countEl.textContent = 0;
+  count = 0;
 }
-
