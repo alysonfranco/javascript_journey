8e9a5731ed6cded3ec78d51081372b303e368a46
Alyson Franco <alysonfranco4@gmail.com>
2023-09-30 00:34:12 -0400

    added increment and save buttons and returned save

--- PassengerCounter/index.js
+++ PassengerCounter/index.js
@@ -1,9 +1,16 @@
-
-let count = 0
-let countEl = document.getElementById("count-el")
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el")
 
 function increment() {
-    count++
-    countEl.innerHTML = count
+    count++;
+    countEl.innerText = count;
     console.log("clicked");
 }
+
+function save() {
+    let entries = count + " - ";
+    saveEl.textContent += entries;
+    console.log(count);
+}
+
