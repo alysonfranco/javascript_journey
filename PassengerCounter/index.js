1cc98a64ed70daea16f99826ea9c71f5d6097387
Alyson Franco <alysonfranco4@gmail.com>
2023-09-29 23:53:03 -0400

    set up JS for button increment

--- PassengerCounter/index.js
+++ PassengerCounter/index.js
@@ -1,1 +1,9 @@
-document.getElementById("count-el").innerText = 5;
+
+let count = 0
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count++
+    countEl.innerHTML = count
+    console.log("clicked");
+}
